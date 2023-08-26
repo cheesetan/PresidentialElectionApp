d333a73a761792958858f24b81e53dd4aa0c0e89
Tristan <tristanchay123@gmail.com>
2023-08-26 09:20:05 +0800

    Initial Commit

---
+++ PresidentialElectionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PresidentialElectionApp
+//
+//  Created by Tristan Chay on 26/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PresidentialElectionApp/PresidentialElectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresidentialElectionAppApp.swift
+//  PresidentialElectionApp
+//
+//  Created by Tristan Chay on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PresidentialElectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
